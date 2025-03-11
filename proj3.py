2e194e282b748fa84f689a4fde8cb280ab4e2e6d
csepregi.mate <csepregi.mate@C19-03>
2025-03-11 11:32:22 +0100

    kesz

--- proj3.py
+++ proj3.py
@@ -1,38 +1,30 @@
-import cv2
+import cv2  # Az OpenCV könyvtár importálása
 
-#############################################
-frameWidth = 640
-frameHeight = 480
-nPlateCascade = cv2.CascadeClassifier("Resources/haarcascade_russian_plate_number.xml")
-minArea = 200
-color = (255,0,255)
-###############################################
-cap = cv2.VideoCapture("Resources/video12.mp4")
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10,150)
-count = 0
+# A rendszám detektáláshoz szükséges Haar-kaskád betöltése
+plateCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_russian_plate_number.xml")  # Rendszám felismeréshez szükséges kaskád fájl betöltése
 
-while True:
-    success, img = cap.read()
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    numberPlates = nPlateCascade.detectMultiScale(imgGray, 1.1, 10)
-    for (x, y, w, h) in numberPlates:
-        area = w*h
-        if area >minArea:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-            cv2.putText(img,"Number Plate",(x,y-5),
-                        cv2.FONT_HERSHEY_COMPLEX_SMALL,1,color,2)
-            imgRoi = img[y:y+h,x:x+w]
-            cv2.imshow("ROI", imgRoi)
+# Kép betöltése
+img = cv2.imread("p4.jpg")  # A 'p1.jpg' képet betöltjük
 
-    cv2.imshow("Result", img)
+# Kép átalakítása szürkeárnyalatossá
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # A képet szürkeárnyalatosra alakítjuk, mivel a Haar-kaskád szürkeárnyalatú képeken működik
 
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite("Resources/Scanned/NoPlate_"+str(count)+".jpg",imgRoi)
-        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
-        cv2.putText(img,"Scan Saved",(150,265),cv2.FONT_HERSHEY_DUPLEX,
-                    2,(0,0,255),2)
-        cv2.imshow("Result",img)
-        cv2.waitKey(500)
-        count +=1
+# Rendszámok detektálása
+plates = plateCascade.detectMultiScale(imgGray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+# A detectMultiScale függvény a képen található rendszámokat próbálja detektálni
+# scaleFactor: Kép méretének csökkentési aránya a vizsgálat során
+# minNeighbors: A rendszámokat elfogadó objektumok számára minimális szomszédsági követelmény
+# minSize: A detektálandó rendszámok minimális mérete
+
+# A detektált rendszámok kiemelése
+for (x, y, w, h) in plates:  # Az 'plates' lista minden egyes elemére (a rendszámok koordinátái)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)  # A rendszámok köré téglalapot rajzolunk (kék színnel, 2-es vastagsággal)
+
+# Az eredmény megjelenítése
+cv2.imshow("rendszam", img)  # Az eredeti képet a detektált rendszámokkal együtt jelenítjük meg
+
+# A program vár egy billentyű lenyomására, mielőtt bezárja az ablakot
+cv2.waitKey(0)  # Az ablak addig nyitva marad, amíg nem nyomunk meg egy billentyűt
+
+# Minden ablak bezárása
+cv2.destroyAllWindows()  # Az összes megnyitott OpenCV ablak bezárása
